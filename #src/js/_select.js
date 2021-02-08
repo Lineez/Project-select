f15b1b54da7551017987fb4408ba458488e3cf04
Egor <qewqew26@mail.ru>
2021-02-08 13:07:55 +0300

    update

--- #src/js/_select.js
+++ #src/js/_select.js
@@ -1,54 +1,59 @@
-function Select() {
-  const select = document.querySelector('._select')
-  const optionS = document.querySelectorAll('._option')
-	// Дата атрибут для 'option'
-	const dataAttribute = 'data-value'
-	// Класс для оболочки 'option'
-	const optionWrapClass = 'form__option-wrap'
-  createSelect(select)
+function Select(selectClass, dataAttribute, optionWrapClass) {
+	const select = document.querySelector(selectClass)
+  const optionS = document.querySelectorAll(`${selectClass} option`)
+  // Дата атрибут для 'option'
+  const data = dataAttribute
+  // Класс для оболочки 'option'
+  const optionWrap = optionWrapClass
 
   // Создание селекта
   function createSelect(select) {
     const newSelect = document.createElement('div')
     select.classList.forEach((el) => {
-      if (el !== '_select') {
+      if (el) {
         newSelect.classList.add(el)
       }
     })
-		createOptionWrap(newSelect)
+    createOptionWrap(newSelect)
     replaceSelect(newSelect)
   }
 
-	function createOptionWrap(newSelect) {
-		const wrap = document.createElement('div')
-		wrap.classList.add(optionWrapClass)
-		newSelect.append(wrap)
-		createOption(wrap)
-	}
+  // Создание оболочки 'option'
+  function createOptionWrap(newSelect) {
+    const wrap = document.createElement('div')
+    wrap.classList.add(optionWrap)
+    newSelect.append(wrap)
+    createOption(wrap)
+  }
 
   // Создание "опции" селекта
   function createOption(wrap) {
     optionS.forEach((el) => {
-			// добавление класса элементу
+      // добавление класса элементу
       const newOption = document.createElement('div')
       el.classList.forEach((c) => {
-        if (c !== '_option') {
+        if (c) {
           newOption.classList.add(c)
         }
       })
-			// добавление 'value' элементу
-			newOption.setAttribute(dataAttribute, el.value)
-			// добавление содержимого элементу
-			newOption.innerText = el.innerText
-			// добавление елемента в селект
+      // добавление 'value' элементу
+      newOption.setAttribute(data, el.value)
+      // добавление содержимого элементу
+      newOption.innerText = el.innerText
+      // добавление елемента в селект
       wrap.append(newOption)
     })
   }
-
+  // Создание селекта
+  createSelect(select)
   // Замена стандартного селекта
   function replaceSelect(newSelect) {
     select.replaceWith(newSelect)
   }
 }
 
-const select = new Select()
+const select = new Select(
+  '.form__select',
+  'data-value',
+  'form__option-wrap'
+)
